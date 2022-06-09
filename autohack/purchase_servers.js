8ab03af1c520e33b8b1a41f4fb3edaf90be71078
shearn89 <alexander.shearn@gmail.com>
2022-06-09 16:32:58 +0100

    tidying

--- autohack/purchase_servers.js
+++ autohack/purchase_servers.js
@@ -11,33 +11,6 @@ export async function main(ns) {
 
 	while (true) {
 		var servers = ns.getPurchasedServers();
-		if (servers.length >= limit) {
-			var smallest = "";
-			var smallestRam = Infinity
-
-			// find smallest server
-			for (let server of servers) {
-				var ram = ns.getServerMaxRam(server);
-				// ns.tprint(`server: ${server}, ram: ${ram}, smallest: ${smallest}, smallestRam: ${smallestRam}`);
-				if (ram < smallestRam) {
-					smallest = server;
-					smallestRam = ram;
-				}
-			}
-
-			// we have the smallest server
-			var newFactor = Math.log2(smallestRam)+1;
-			ns.print(`found smallest server ${smallest}, with ${smallestRam} RAM (2^${Math.log2(smallestRam)}), replacing at factor ${newFactor}`);
-			if (ns.ps(smallest).length > 0) {
-				ns.print("server is running scripts, skipping");
-			} else {
-				ns.print("deleting server");
-				ns.deleteServer(smallest);
-			}
-			factor = newFactor;
-			await ns.write(dataFile, factor, "w");
-		}
-
 		while (servers.length < limit) {
 			var cash = ns.getServerMoneyAvailable("home");
 			var ram = Math.pow(2, factor);
--- autohack/upgrade_servers.js
+++ autohack/upgrade_servers.js
@@ -17,7 +17,12 @@ export async function main(ns) {
 
 			// find smallest server
 			for (let server of servers) {
+				if (ns.ps(server).length > 0) {
+					ns.tprint("server is running scripts, skipping");
+					continue;
+				}
 				var ram = ns.getServerMaxRam(server);
+
 				// ns.tprint(`server: ${server}, ram: ${ram}, smallest: ${smallest}, smallestRam: ${smallestRam}`);
 				if (ram < smallestRam) {
 					smallest = server;
@@ -26,16 +31,18 @@ export async function main(ns) {
 			}
 
 			// we have the smallest server
-			var newFactor = Math.log2(smallestRam)+1;
-			ns.print(`found smallest server ${smallest}, with ${smallestRam} RAM (2^${Math.log2(smallestRam)}), replacing at factor ${newFactor}`);
+			var smallestFactor = Math.log2(smallestRam)+1;
+			ns.tprint(`found smallest server ${smallest}, with ${smallestRam} RAM (2^${Math.log2(smallestRam)}), replacing at factor ${smallestFactor}`);
 			if (ns.ps(smallest).length > 0) {
-				ns.print("server is running scripts, skipping");
+				ns.tprint("server is running scripts, skipping");
 			} else {
-				ns.print("deleting server");
+				ns.tprint("deleting server");
 				ns.deleteServer(smallest);
 			}
-			factor = newFactor;
-			await ns.write(dataFile, factor, "w");
+			if (smallestFactor > factor) {
+				factor = smallestFactor;
+				await ns.write(dataFile, factor, "w");
+			}
 		}
 		await ns.sleep(100);
 		while (servers.length < limit) {
