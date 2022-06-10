7529d620c0f1b542795df72483e6448a0df546b6
shearn89 <alexander.shearn@gmail.com>
2022-06-10 16:28:15 +0100

    removing extra print

--- smart/smart_weaken.js
+++ smart/smart_weaken.js
@@ -14,7 +14,6 @@ export async function main(ns) {
     }
     const target = flags['target'];
     const batchTag = flags['batchtag'];
-    ns.tprint(target);
 
     var weakenThreads = get_weaken_threads(ns, target);
     var scripts = [weakenScript];
