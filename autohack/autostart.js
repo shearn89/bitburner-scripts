27dc80f0e4696517b7203ede18363005da26c7d2
shearn89 <alexander.shearn@gmail.com>
2022-06-15 20:40:08 +0100

    tweaking autostart

--- autohack/autostart.js
+++ autohack/autostart.js
@@ -5,9 +5,9 @@ export async function main(ns) {
     ns.disableLog("ALL");
 
     // run spider.js, hack what we can. Should start with 3 exploits.
-    var spiderPid = ns.run("/spider.js");
+    var spiderPid = ns.run("/spider.js", 1);
     // give it a second
-    ns.sleep(2*1000);
+    await ns.sleep(2*1000);
 
     const maxRunning = 4;
     var running = 0;
@@ -27,28 +27,31 @@ export async function main(ns) {
         if ((myHackLevel > (serverHackLevel/2) || serverHackLevel <= 1) && hasRoot && (runningPids.length < maxRunning)) {
             ns.print(`passed checks: ${runningPids.length} scripts running. running hack on ${target}`);
             running += 1;
-            var pid = ns.run("/smart/smart_meta_loop.js", "--target", target);
+            var pid = ns.run("/smart/smart_meta_loop.js", 1, "--target", target);
             runningPids.push({"target": target, "pid": pid})
             ns.print(runningPids);
+        } else if ((myHackLevel > (serverHackLevel/2)) && hasRoot && (runningPids.length >= maxRunning)) {
+            // if the target is better, kill the earliest pid aka weakest target.
+            ns.print(`${target} hack level (${serverHackLevel}) is better than our current, killing one script`);
+            var pidToKill = runningPids[0]['pid']
+            ns.kill(pidToKill, ns.getHostname())
+            // so next time there'll be space
+            targetList.push(target);
         } else {
             // return to the list, ready for retry
             targetList.push(target);
         }
         
-        // if the target is better, kill the earliest pid aka weakest target.
-        if ((myHackLevel > (serverHackLevel/2)) && hasRoot && (runningPids.length >= maxRunning)) {
-            ns.print(`${target} hack level (${serverHackLevel}) is better than our current, killing one script`);
-            var pidToKill = runningPids[0]['pid']
-            ns.kill(pidToKill, ns.getHostName())
-            // so next time there'll be space
-        }
 
-        if (runningPids.length < maxRunning) {
+        if ((runningPids.length < maxRunning) && hasRoot) {
             ns.print(`limit (${maxRunning}) not reached, looping`);
             await ns.sleep(1000);
+        } else if (runningPids.length < maxRunning) {
+            ns.print(`limit (${maxRunning}) not reached, but no better targets, sleeping...`);
+            await ns.sleep(1000*60);
         } else {
             ns.print(`${maxRunning} hacks running, no better targets, sleeping...`);
-            await ns.sleep(1000*60*10);
+            await ns.sleep(1000*60);
         }
     }
 }
